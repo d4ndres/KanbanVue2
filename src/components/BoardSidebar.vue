e69318fa4d9614782619842df67eac4760287a6b
FredyCarvajal <clanbogota@gmail.com>
2023-11-10 20:26:24 -0500

    lateral dinamico

--- src/components/BoardSidebar.vue
+++ src/components/BoardSidebar.vue
@@ -6,6 +6,11 @@ export default {
   components: {
     ManagmentBoard,
   },
+  data() {
+    return {
+      isActive: false,
+    };
+  },
 };
 </script>
 
@@ -13,14 +18,23 @@ export default {
   <div
     class="container | min-h-3/4 p-3 my-2 mx-auto | bg-bg rounded-x1 shadow-2xl | flex"
   >
+    <div
+      @click="isActive = !isActive"
+      class="bg-gris py-1 px-3 w-[75px] rounded-md | text-morado text-center | font-bold | absolute | cursor-pointer | bottom-[-50px] | z-10"
+    >
+      {{ isActive }}
+    </div>
     <!-- Left side -->
-    <section class="w-1/2 p-3 mr-2 | bg-oscuro | text-morado | font-bold">
-      <!-- Search notes -->
-
-      <!-- Note list -->
+    <section
+      :class="{ 'w-[0%] | opacity-0': isActive == true }"
+      class="w-[20%] mr-2 | bg-oscuro | text-morado | font-bold | duration-500"
+    >
       <p>Note list</p>
     </section>
-    <section>
+    <section
+      :class="{ 'w-[100%]': isActive == true }"
+      class="w-[80%] | duration-500"
+    >
       <ManagmentBoard />
     </section>
   </div>
